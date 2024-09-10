0d4c40fa42635a42945408f5a984c7cbc41d026b
yangfangyuan0102 <49721176+yangfangyuan0102@users.noreply.github.com>
2024-09-10 10:03:59 +0800

    Update 11run_codeml_parallel.sh

--- 11run_codeml_parallel.sh
+++ 11run_codeml_parallel.sh
@@ -2,6 +2,6 @@
 work_dir=/mnt/dataset/moth/Analysis/dsdn/all_leafrooler/abcc1_aln
 
 #11codeml.sh
-find ${work_dir} -type f -name "*fasta" | parallel   -j32 "./codeml.sh {}" 
+realpath ${work_dir}/*fasta | parallel   -j32 "./codeml.sh {}" 
 #11chi2.sh
-find ${work_dir} -type f -name "*fasta" | parallel   -j32 "./chi2.sh {}"
+realpath ${work_dir}/*fasta | parallel   -j32 "./chi2.sh {}"
